98adad9969d1724783e77132204f840b98cc15ed
Code of the Future <75938783+code-of-the-future@users.noreply.github.com>
2021-01-12 11:59:55 +0000

    Add files via upload

---
+++ TikTok_Liker.py
@@ -0,0 +1,18 @@
+# Import relevant modules
+import time
+import pyautogui
+
+# Give some time before python runs this file
+time.sleep(5)
+
+# Finding your mouse's current position on the screen
+# print(pyautogui.position())
+
+# The TikTok is located at x=550, y=520
+
+pyautogui.moveTo(550, 520)
+for i in range(5):
+    time.sleep(0.5)
+    pyautogui.doubleClick()
+    time.sleep(0.5)
+    pyautogui.scroll(-16)
